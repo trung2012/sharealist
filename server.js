ead412b274de6eef42f70951b244a7001a77888e
Trung Pham <trungpham2012@gmail.com>
2019-10-23 10:29:30 -0500

    safari issue

--- server.js
+++ server.js
@@ -114,9 +114,9 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  // });
 };
 
 const PORT = process.env.PORT || 5000;
