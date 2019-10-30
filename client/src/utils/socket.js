b540494ab7d742215093609bfcb8c037ee25b138
Trung Pham <trungpham2012@gmail.com>
2019-10-29 22:59:36 -0500

    socket update

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,5 +1,5 @@
 import socketioClient from 'socket.io-client';
 
-const socket = socketioClient('http://localhost:5000/', { transports: ['websocket'] });
+const socket = socketioClient('/', { transports: ['websocket'] });
 
 export default socket;
